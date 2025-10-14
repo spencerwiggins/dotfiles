7bbda0748b1bc7928375ef46efd7d0171bf805ca
cn100344 <cn100344@cnclqhh49q39l.centene.com>
2025-10-14 15:22:26 -0400

    added hammer

---
+++ hammerspoon/init.lua
@@ -0,0 +1,15 @@
+hs.hotkey.bind({ "cmd", "ctrl" }, "t", function()
+	hs.application.launchOrFocus("iTerm")
+end)
+
+hs.hotkey.bind({ "cmd", "ctrl" }, "b", function()
+	hs.application.launchOrFocus("Google Chrome")
+end)
+
+hs.hotkey.bind({ "cmd", "ctrl" }, "c", function()
+	hs.application.launchOrFocus("Microsoft Teams")
+end)
+
+hs.hotkey.bind({ "cmd", "ctrl" }, "m", function()
+	hs.application.launchOrFocus("Microsoft Outlook")
+end)
